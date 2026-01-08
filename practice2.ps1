2fe21b1dd3bf4fc48c6fe6aa8ddc74627c751e3e
Antero Poder <antero.poder@voco.ee>
2026-01-08 08:59:27 +0200

    PS algajate eBook harjutus2

---
+++ practice2.ps1
@@ -0,0 +1,14 @@
+$hashtable1 = @{Name="John", "Joe", "Mary";DaysWorked=12, 20, 18}
+$hashtable2 = @{Name="John", "Joe", "Mary";SalaryPerDay=100, 120, 150}
+
+$john_salary = $hashtable1.DaysWorked[0] * $hashtable2.SalaryPerDay[0]
+$joe_salary = $hashtable1.DaysWorked[1] * $hashtable2.SalaryPerDay[1]
+$mary_salary = $hashtable1.DaysWorked[2] * $hashtable2.SalaryPerDay[2]
+
+$hashtable3 = @{Name="John", "Joe", "Mary";Salary=$john_salary, $joe_salary, $mary_salary}
+
+echo $hashtable1
+echo "-------------------------------------------------"
+echo $hashtable2
+echo "-------------------------------------------------"
+echo $hashtable3
