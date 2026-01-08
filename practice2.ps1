15ed622bf8edbd651cadb34f9034529b4e0c34ad
Antero Poder <antero.poder@voco.ee>
2026-01-08 10:03:00 +0200

    Parandatud harjutus2

--- practice2.ps1
+++ practice2.ps1
@@ -1,11 +1,11 @@
-$hashtable1 = @{Name="John", "Joe", "Mary";DaysWorked=12, 20, 18}
-$hashtable2 = @{Name="John", "Joe", "Mary";SalaryPerDay=100, 120, 150}
+$hashtable1 = [ordered]@{Name="John", "Joe", "Mary";DaysWorked=12, 20, 18}
+$hashtable2 = [ordered]@{Name="John", "Joe", "Mary";SalaryPerDay=100, 120, 150}
 
 $john_salary = $hashtable1.DaysWorked[0] * $hashtable2.SalaryPerDay[0]
 $joe_salary = $hashtable1.DaysWorked[1] * $hashtable2.SalaryPerDay[1]
 $mary_salary = $hashtable1.DaysWorked[2] * $hashtable2.SalaryPerDay[2]
 
-$hashtable3 = @{Name="John", "Joe", "Mary";Salary=$john_salary, $joe_salary, $mary_salary}
+$hashtable3 = [ordered]@{Name="John", "Joe", "Mary";Salary=$john_salary, $joe_salary, $mary_salary}
 
 echo $hashtable1
 echo "-------------------------------------------------"
