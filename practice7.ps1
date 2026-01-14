c46dd56bf20d8aee5d76e8a41f8e11d9077e5467
Antero Poder <antero.poder@voco.ee>
2026-01-14 15:41:50 +0200

    Harjutus 7

---
+++ practice7.ps1
@@ -0,0 +1,13 @@
+﻿$students = 20
+$colors = @("red", "green", "yellow", "blue")
+$result = @()
+
+for ($i = 1; $i -le $students; $i++) {
+$group = Get-Random -InputObject $colors
+$result += [PSCustomObject]@{
+RoleNumber = $i
+Group = $group
+}
+}
+
+$Result | Format-Table
