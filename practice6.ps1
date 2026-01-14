b4c5231f361772532786ab1d3569b81453bd5ecc
Antero Poder <antero.poder@voco.ee>
2026-01-14 15:17:22 +0200

    Harjutus 6

---
+++ practice6.ps1
@@ -0,0 +1,22 @@
+﻿$result = @()
+$ages = Import-Csv C:\Users\Antero\powershellscript\ps_ebook\ages.txt
+
+$ages | ForEach-Object {
+
+if ([int]$_.Age -ge 4 -and [int]$_.Age -le 10) {
+$School = "Junior"
+}
+
+elseif ([int]$_.Age -ge 11 -and [int]$_.Age -le 17) {
+$school = "Senior"
+}
+
+$result += [PSCustomObject]@{
+Name = $_.Name
+School = $School
+}
+}
+
+$result
+
+$result | Out-File "C:\Users\Antero\powershellscript\ps_ebook\School"
