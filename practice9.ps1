6ca5416946285859c5faa4e1bc6198681bf38bfd
Antero Poder <antero.poder@voco.ee>
2026-02-03 16:48:20 +0200

    Harjutus 9

---
+++ practice9.ps1
@@ -0,0 +1,8 @@
+﻿$loop_count = 0
+do {
+    Write-Host "Notepad is running"
+    $loop_count+=1
+    Start-Sleep 1
+}while(Get-Process -Name notepad -ErrorAction SilentlyContinue)
+
+Write-Host "The loop ran $loop_count times"
