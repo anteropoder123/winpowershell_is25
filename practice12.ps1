d7d07ad50c4f23dd76802be47b93f30ffc361f67
Antero Poder <antero.poder@voco.ee>
2026-02-04 11:29:05 +0200

    Harjutus 12

---
+++ practice12.ps1
@@ -0,0 +1,20 @@
+﻿#Parameetrita
+function Running-Services{
+    $running = Get-Service | where{$_.Status -eq "running"}
+    Write-Host "Running Services:" $running.Count
+}
+
+function Stopped-Services{
+    $stopped = Get-Service | where{$_.Status -eq "stopped"}
+    Write-Host "Stopped services:" $stopped.Count
+}
+
+#Parameetriga
+function Services-func{
+    param(
+        [string]$status
+    )
+
+    $running = Get-Service | where{$_.Status -eq $status}
+    Write-Host "Running services:" $running.Count
+}
